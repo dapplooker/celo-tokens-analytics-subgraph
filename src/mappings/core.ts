18eab0b3f31394657a4a5d765eb91af8efc7c13f
Himanshu Mandwal <himanshumandwal2108@gmail.com>
2021-12-27 22:12:13 +0530

    added logs and added some tokens to subgraph

--- src/mappings/core.ts
+++ src/mappings/core.ts
@@ -20,11 +20,12 @@ import {
 } from "./helpers";
 
 export function handleTransfer(event: ethereum.Event): void {
+  log.info("handleTransfer 1 ={}",[event.block.number.toString()]);
   let tokenAddress = event.address;
   let token = Token.load(event.address.toHexString());
   if (!token) {
+    log.warning("handleTransfer::inside token load case = {}",[event.address.toHexString()]);
     token = new Token(event.address.toHexString());
-    log.warning('here {}',[event.address.toHexString()]);
     token.symbol = fetchTokenSymbol(tokenAddress);
     token.name = fetchTokenName(tokenAddress);
     token.totalSupply = fetchTokenTotalSupply(tokenAddress);
@@ -36,6 +37,7 @@ export function handleTransfer(event: ethereum.Event): void {
     token.lastUpdatedTimestamp = event.block.timestamp.toI32();
     token.save();
   }
+  log.info("handleTransfer 2 ={}",[event.block.number.toString()]);
   token.txCount = token.txCount.plus(ONE_BI);
   createUser(event.parameters[0].value.toAddress(), event.address.toHexString());
   createUser(event.parameters[1].value.toAddress(), event.address.toHexString());
